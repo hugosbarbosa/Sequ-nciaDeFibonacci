37ae9144da05d285ca03799f908c6962029638a1
hugobarbosa <125314621+hugosbarbosa@users.noreply.github.com>
2024-11-08 22:18:58 -0300

    100%

--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ namespace SequênciaDeFibonacci
     {
         static void Main(string[] args)
         {
+            int a = 0, b = 1, c = 0;
+
+            Console.Write("Selecione a quantidade de valores: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sequência de Fibonacci com " + valores + " valores:");
+
+            Console.WriteLine();
+
+            for (int i = 0; i < valores; i++)
+            {
+                if (i < valores - 1)
+                {
+                    Console.Write(a + ",");
+
+                }
+                else
+                {
+                    Console.Write(a);
+                }
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            Console.ReadKey();
         }
     }
 }
